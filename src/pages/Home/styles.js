2eb1b615d4baf2c5806c733956b664840bcebe91
thailajf <thailajf@hotmail.com>
2020-01-10 23:20:09 -0300

    Rockeatshoes

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const ProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+
+    img {
+      align-self: center;
+      max-width: 250px;
+    }
+    /* > é para especificar e forcar q essa estilização eer do da strong desse li */
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      overflow: hidden;
+      /* center no margin faz com q ele se ajuste ex no btn mesmo se a legenda for enorme a margin-top vai colocar margin no topo suficiente em cada btn p els sempre ficarem alinhados*/
+      margin-top: auto;
+
+      display: flex;
+      align-items: center;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
+      }
+
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+  }
+`;
---
+++ src/store/modules/cart/reducer.js
@@ -0,0 +1,32 @@
+import produce from 'immer';
+import { Number } from 'core-js';
+
+export default function cart(state = [], action) {
+  console.log(state);
+  switch (action.type) {
+    case '@cart/ADD_SUCCESS':
+      return produce(state, draft => {
+        const { product } = action;
+        draft.push(product);
+      });
+    case '@cart/REMOVE':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+        if (productIndex >= 0) {
+          //o index q quer removr e qtd de elementos q quer remover
+          draft.splice(productIndex, 1);
+        }
+      });
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
+    default:
+      return state;
+  }
+}
